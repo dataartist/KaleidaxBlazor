7a088716db73f4537f60815f3ff8e472fe8bb356
Todd Knudsen <toddk@dataartist.net>
2020-04-28 15:44:45 -0600

    Add project files.

---
+++ KaleidaxBlazorApp1/Extensions.cs
@@ -0,0 +1,10 @@
+﻿namespace KaleidaxBlazorApp1
+{
+    public static class Extensions
+    {
+        public static string ToggleImage(this string image, bool forceOff = false)
+        {
+            return image.IndexOf("Selected") >= 0 || forceOff ? image.Replace("Selected", "") : image.Replace(".", "Selected.");
+        }
+    }
+}
